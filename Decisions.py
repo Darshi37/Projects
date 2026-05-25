10c535d5245e016b56a73643fc096cf18b9ee6ff
Darshi Hewa <darshihep@gmail.com>
2026-05-25 15:17:04 -0400

    Initial commit

---
+++ Decisions.py
@@ -0,0 +1,10 @@
+#for loop
+#range(5) means start at 0, stop before 5
+for i in range(5):
+    print(i)
+    if i > 2:
+        print("Bigger than two")
+print("Done with i")
+
+#Example of nested blocks/ block inside a block.
+#Indentation decides which inside which block
