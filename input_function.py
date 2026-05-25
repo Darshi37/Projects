84b9ea0a4916c1561b257c2e4f1219af50ef7a95
Darshi Hewa <darshihep@gmail.com>
2026-05-25 16:37:29 -0400

    Initial commit

---
+++ input_function.py
@@ -0,0 +1,18 @@
+name = input("What is your name?")
+print("The name is:",name)
+
+age = input("What is your age?")
+print("The age is:",age)
+age = int(age)
+print(type(age))
+
+salary_hour = int(input("What is your salary per hour?"))
+print(type(salary_hour))
+
+value = int(input("What is your lucky number?"))
+while value > 0:
+    value = value - 1
+    print(value,end="")
+    if value > 0:
+        print(",", end="")
+print("\nall numbers are printed")
