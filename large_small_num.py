ac4c5a4fbbce924570a05aa48a2935c0107f6fb3
Darshi Hewa <darshihep@gmail.com>
2026-05-25 20:15:18 -0400

    Initial stage of the file

---
+++ large_small_num.py
@@ -0,0 +1,26 @@
+largest = 0
+smallest = None
+
+while True:
+    number = input("Enter a number: ")
+    if number == "done":
+        break
+
+    try:
+        number = int(number)
+        if number > largest:
+            largest = number
+        if smallest == None:
+            smallest = number
+        if number < smallest:
+            smallest = number
+    except:
+        print("Invalid input")
+
+print("maximum is:", largest)
+print("minimum is:", smallest)
+
+
+
+
+
